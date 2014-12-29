4d9dfa7a374ed4969315462553f73bb5075eb7f8
Angel Vasilev <enzeru@mail.bg>
2014-12-29 22:05:25 +0200

    Integrated Ninject in the Web project

--- Source/Web/ForumSystem.Web/Controllers/HomeController.cs
+++ Source/Web/ForumSystem.Web/Controllers/HomeController.cs
@@ -13,12 +13,6 @@ namespace ForumSystem.Web.Controllers
     {
         private IRepository<Post> posts;
 
-        public HomeController()
-            : this(new GenericRepository<Post>(new ApplicationDbContext()))
-        {
-
-        }
-
         public HomeController(IRepository<Post> posts)
         {
             this.posts = posts;
