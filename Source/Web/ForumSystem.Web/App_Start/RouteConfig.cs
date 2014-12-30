d4d93b2238040c475efa9d9c658f8753a18fdc98
Angel Vasilev <enzeru@mail.bg>
2014-12-30 20:44:22 +0200

    Added routes for threads

--- Source/Web/ForumSystem.Web/App_Start/RouteConfig.cs
+++ Source/Web/ForumSystem.Web/App_Start/RouteConfig.cs
@@ -13,6 +13,20 @@ namespace ForumSystem.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // /threads/tagged/javascript
+            routes.MapRoute(
+                name: "Threads by tag",
+                url: "threads/tagged/{tag}",
+                defaults: new { controller = "Threads", action = "GetByTag" }
+            );
+
+            // /threads/threadID/thread-title
+            routes.MapRoute(
+                name: "Display thread",
+                url: "threads/{id}/{url}",
+                defaults: new { controller = "Threads", action = "Display" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Source/Web/ForumSystem.Web/Controllers/ThreadsController.cs
@@ -0,0 +1,37 @@
+﻿using ForumSystem.Web.InputModels.Threads;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumSystem.Web.Controllers
+{
+    public class ThreadsController : Controller
+    {
+        // /threads/threadID/thread-title
+        public ActionResult Display(int id, string url, int page = 1)
+        {
+            return View();
+        }
+
+        // /threads/threadID/thread-title
+        public ActionResult GetByTag(string tag)
+        {
+            return View();
+        }
+
+        // GET - POST - REDIRECT
+        [HttpGet]
+        public ActionResult Ask()
+        {
+            return Content("Get");
+        }
+
+        [HttpPost]
+        public ActionResult Ask(AskInputModel input)
+        {
+            return Content("Post");
+        }
+	}
+}
